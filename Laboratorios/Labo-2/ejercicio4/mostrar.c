69e8d98b8899db2d48e6289bc7267745b337836b
A.L.K <alejandrobarrachina.02@gmail.com>
2022-10-03 10:05:16 +0200

    mostrar hecho

--- Laboratorios/Labo-2/ejercicio4/mostrar.c
+++ Laboratorios/Labo-2/ejercicio4/mostrar.c
@@ -7,23 +7,72 @@ int main(int argc, char *argv[])
 	long n = 0;
 	int N_FLAG = 0;
 	int readLast = 0;
-	int c = getopt(argc, argv, "en:");
-	if (c == -1)
+	int c;
+	char *filename = argv[1];
+
+	while (1)
 	{
-		perror("getopt");
+		c = getopt(argc, argv, "en:");
+
+		if (c == -1)
+		{
+			break;
+		}
+
+		switch (c)
+		{
+		case 'n':
+			n = strtol(optarg, NULL, 10);
+			N_FLAG = 1;
+			break;
+		case 'e':
+			readLast = 1;
+
+		default:
+			break;
+		}
+	}
+
+	if (readLast == 1 && N_FLAG == 0)
+	{
+		perror("-e tiene que tener un valor -n");
 		exit(EXIT_FAILURE);
 	}
-	switch (c)
-	{
-	case 'n':
-		n = strtol(optarg, NULL, 10);
-		N_FLAG = 1;
-		break;
-	case 'e':
-		readLast = 1;
 
-	default:
-		break;
+	char ch;
+	char *chs;
+	FILE *ptr = fopen(filename, "r");
+	if (NULL == ptr)
+	{
+		perror("file can't be opened");
+		exit(EXIT_FAILURE);
+	}
+
+	if (readLast == 1)
+	{
+		fseek(ptr, 0, SEEK_END);
+		int length = ftell(ptr);
+		fseek(ptr, (length - n - 1), SEEK_SET);
+	}
+
+	int i = 0;
+	if (N_FLAG == 0 || N_FLAG == 1 && readLast == 1)
+	{
+		while (ch != EOF)
+		{
+			ch = fgetc(ptr);
+			printf("%c", ch);
+		}
+	}
+	else
+	{
+		while (ch != EOF && i < n)
+		{
+			ch = fgetc(ptr);
+			printf("%c", ch);
+			++i;
+		}
+		printf("\n");
 	}
 
 	return 0;
