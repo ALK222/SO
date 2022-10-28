f382139e3bb813b6d7c98750c3688ac1f5fef257
A.L.K <alejandrobarrachina.02@gmail.com>
2022-10-28 10:36:17 +0200

    Ejercicio 5

--- Laboratorios/Labo-3/ejercicio5/practica_2_5_inicial.c
+++ Laboratorios/Labo-3/ejercicio5/practica_2_5_inicial.c
@@ -8,34 +8,44 @@
 
 int main(void)
 {
-    int fd1,fd2,i,pos;
+    int fd1, fd2, i, pos;
     char c;
     char buffer[6];
 
     fd1 = open("output.txt", O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
     write(fd1, "00000", 5);
-    for (i=1; i < 10; i++) {
+    for (i = 1; i < 10; i++)
+    {
         pos = lseek(fd1, 0, SEEK_CUR);
-        if (fork() == 0) {
+        if (fork() == 0)
+        { /* Parent */
+            if (i < 9)
+            {
+                printf("coso\n");
+                write(fd1, "00000", 5);
+
+                lseek(fd1, 0, SEEK_CUR);
+            }
+        }
+        else
+        {
             /* Child */
-            sprintf(buffer, "%d", i*11111);
+            sprintf(buffer, "%d", i * 11111);
             lseek(fd1, pos, SEEK_SET);
             write(fd1, buffer, 5);
             close(fd1);
             exit(0);
-        } else {
-            /* Parent */
-            lseek(fd1, 5, SEEK_CUR);
         }
     }
 
-	//wait for all childs to finish
-    while (wait(NULL) != -1);
+    // wait for all childs to finish
+    while (wait(NULL) != -1)
+        ;
 
     lseek(fd1, 0, SEEK_SET);
     printf("File contents are:\n");
     while (read(fd1, &c, 1) > 0)
-        printf("%c", (char) c);
+        printf("%c", (char)c);
     printf("\n");
     close(fd1);
     exit(0);
