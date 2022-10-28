f99e5d9f35c955c479d45d661cfcc327ae913a21
A.L.K <alejandrobarrachina.02@gmail.com>
2022-10-28 10:11:37 +0200

    Ejercicios 2 y 3

--- Laboratorios/Labo-3/ejercicio3/hilos.c
+++ Laboratorios/Labo-3/ejercicio3/hilos.c
@@ -1,13 +1,19 @@
 #include <stdlib.h>
+#include <pthread.h>
 #include <stdio.h>
 
 void *thread_usuario(void *arg)
 {
-
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
+
+	if (pthread_create() != 0)
+	{
+		perror("create");
+		exit(EXIT_FAILURE);
+	}
 
 	return 0;
 }
