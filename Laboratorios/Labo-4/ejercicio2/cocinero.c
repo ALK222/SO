9f0cf775ab15890cf834af91086aa75d558fef78
Rehis <rehis94@gmail.com>
2022-11-30 11:59:34 +0100

    Cocinero y salvaje
    Se complentan teoricamente cocinero y salvaje falta el main

--- Laboratorios/Labo-4/ejercicio2/cocinero.c
+++ Laboratorios/Labo-4/ejercicio2/cocinero.c
@@ -9,8 +9,8 @@
 #define MAX_BUFFER 1024 /*tamaño del buffer*/
 #define RACIONES 1000   /*Datos a producir*/
 
-sem_t *elements;
-sem_t *holes;
+sem_t *elements; // Elementos en el buffer
+sem_t *holes; //huecos en el buffer
 
 int putServingsInPot()
 {
@@ -18,18 +18,43 @@ int putServingsInPot()
     return r;
 }
 
-void Cocinero()
+void Cocinero(int *buffer)
 {
     int pos = 0;
     int dato;
     int i;
+    for(i = 0; i < RACIONES; i++){
+        dato = putServingsInPot();
+        buffer[pos] = dato;
+        pos = (pos+1) %MAX_BUFFER;
+        sem_post(elements);
+
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    while (!finish)
-    {
-        putServingInPot();
-    }
+    //Aqui preparamos el buffer y los semaforos para producir
+    int shd, *bff;
+
+    shd = open("BUFFER", O_CREAT|O_RDWR, 0700);
+    ftruncate(shd, MAX_BUFFER *sizeof(int));
+    
+    bff = (int*)mmap(NULL,MAX_BUFFER *sizeof(int), PROT_WRITE, MAP_SHARED, shd,0);
+    elements = sem_open("/ELEMENTS", O_CREAT, 0700,0);
+    holes = sem_open("/HOLES", O_CREAT, 0700,MAX_BUFFER);
+    //llamamos a cocinero para que cree todas las raciones necesarias hasta llenar
+    Cocinero(bff);
+    //una vez relleno se procede a desmapear y cerrar los semaforos
+
+    munmap(bff, MAX_BUFFER *sizeof(int));
+    close(shd);
+    unlink("BUFFER");
+    sem_close(elements);
+    sem_close(holes);
+    sem_unlink("/ELEMENTS");
+    sem_unlink("/HOLES");
+
+
     exit(EXIT_SUCCESS);
 }
--- Laboratorios/Labo-4/ejercicio2/salvajes.c
+++ Laboratorios/Labo-4/ejercicio2/salvajes.c
@@ -1,18 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <pthread.h>
+#include <semaphore.h>
 #include <fcntl.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <sys/stat.h>
 #include <string.h>
+#include <sys/mman.h>
+
+#define MAX_BUFFER 1024 // tamanno del buff
+#define RACIONES 1000
+
+sem_t *elements; //Elementos del buffer
+sem_t *holes; //Huecos en el buff;
+
+void eat(int dato){
+    printf("se ha comido %d \n", dato);
+
+}
+void getServingFromPot(int *buffer){
+    int pos = 0;
+    int i, dato;
+    for(i = 0; i < RACIONES; i++){
+        sem_wait(elements);
+        dato=buffer[pos];
+        pos = (pos+1) %MAX_BUFFER;
+        sem_post(holes);
+        eat(dato);
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    for (int i = 0; i < NUMITER; i++)
-    {
-        getServingFromPot();
-        eat();
-    }
+    //CREAR Y ABRIR LOS SEMAFOROS 
+    int shd, *buffer;
+    shd= open("BUFFER", O_RDONLY);
+    buffer= (int*) mmap(NULL,MAX_BUFFER * sizeof(int), PROT_READ, MAP_SHARED, shd,0);
+    elements= sem_open("/ELEMENTS",0),
+    holes = sem_open("/HOLES",0);
+    //LOs salvajes comen
+    getServingFromPot(buffer);
+
+    // SE CIERRAN LOS SEMAFOROS y memoria compartida
+    munmap(buffer, MAX_BUFFER * sizeof(int));
+    close(shd);
+    sem_close(elements);
+    sem_close(holes);
     exit(EXIT_SUCCESS);
 }
